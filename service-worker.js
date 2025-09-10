83255a3d6c8cd1a33efa4bae3ad330a5b0c65151
FLAVIO RAFAEL CARDOZO <125320966+Flavero85@users.noreply.github.com>
2025-09-10 15:38:17 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'suporte-n2-cache-v2'; // Versão do cache atualizada
+const urlsToCache = [
+  '/',
+  'index.html',
+  'style.css',
+  'script.js',
+  'logo.png',
+  'icon-192.png',
+  'icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  // URLs do Firebase SDK
+  "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js",
+  "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js"
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
